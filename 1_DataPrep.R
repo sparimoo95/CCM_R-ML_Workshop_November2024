b5d07c171ee422d55a2d032b591dd4b2dfaec446
sparimoo95 <31860708+sparimoo95@users.noreply.github.com>
2024-11-04 12:45:31 -0500

    upload scripts

--- 1_DataPrep.R
+++ 1_DataPrep.R
@@ -5,7 +5,7 @@ setwd("/Users/shireenparimoo/Documents/Teaching/R Workshop - November 2024/data/
 #
 ## 00. Load libraries ----------------------------------------------------
 
-# install.packages("tidyverse", "caret", "pROC", "caTools", "randomForest", "kohonen", "recipes", "tidymodels", "factoextra", "rpart.plot")
+# install.packages("tidyverse", "caret", "pROC", "caTools", "randomForest", "tidymodels", "factoextra", "rpart.plot")
 library(tidyverse) # we will use this library, which comes with its own syntax
 library(pROC) # plot ROC curves and calculate area under the curve (AUC)
 library(caret) # specify models
@@ -13,7 +13,6 @@ library(caTools) # split dataset into training and test sets
 library(e1071) # svm
 library(randomForest) # random forest model
 library(tidymodels)
-library(kohonen) # wine dataset
 library(factoextra) # k means viz
 library(corrplot) # correlation plot
 library(rpart.plot) # random forest plots
@@ -23,10 +22,6 @@ library(rpart.plot) # random forest plots
 ## 01. Import data ------------------------------------------------------
 
 raw_heart_df <- read.csv("framingham.csv", header = TRUE, sep = ",")
-# raw_beer_df <- read.csv("beer_profile_and_ratings.csv", header = TRUE, sep = ",")
-# data(wines)
-# raw_wines_df <- as.data.frame(wines) # features
-# wine_classes_df <- as.data.frame(vintages) # labels
 
 #
 
@@ -73,23 +68,3 @@ prepped_heart_df_cluster <- prepped_heart_df %>%
 corrplot(cor(prepped_heart_df_cluster))
 
 #
-
-# # Wines Dataset ------------------------------------------------
-# 
-# # combine the wine and vintages datasets and do some renaming
-# prepped_wines_df <- cbind(raw_wines_df, vintages) %>% 
-#   # rename some of the columns so there are no spaces in the names
-#   dplyr::rename(malic_acid = `malic acid`,
-#                 ash_alkalinity = `ash alkalinity`,
-#                 total_phenols = `tot. phenols`,
-#                 non_flav_phenols = `non-flav. phenols`,
-#                 color_intensity = `col. int.`,
-#                 color_hue = `col. hue`) 
-# 
-# # normalize the wines dataset for clustering (after removing the vintages column)
-# norm_wines_df <- as.data.frame(scale(prepped_wines_df %>% select(-vintages)))
-# 
-# # look at correlations in the clustering dataset
-# corrplot(cor(norm_wines_df))
-# 
-# #
